b4f27dca3eae5d565d99b7f9069df8eb187e9b90
JoseUrdiales <49536509+JoseUrdiales@users.noreply.github.com>
2024-01-12 13:32:43 -0500

    Add files via upload

--- patronObserver.py
+++ patronObserver.py
@@ -23,3 +23,24 @@ class SujetoNoticias:
 class ObservadorNoticias:
     def actualizar(self, noticia):
         pass
+
+
+# Implementación concreta del Observador
+class Suscriptor(ObservadorNoticias):
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def actualizar(self, noticia):
+        print(f'{self.nombre} ha recibido la noticia: "{noticia}"')
+
+
+# Uso del patrón Observer
+sujeto_noticias = SujetoNoticias()
+
+suscriptor1 = Suscriptor("Suscriptor 1")
+suscriptor2 = Suscriptor("Suscriptor 2")
+
+sujeto_noticias.agregar_observador(suscriptor1)
+sujeto_noticias.agregar_observador(suscriptor2)
+
+sujeto_noticias.publicar_noticia("¡Nueva noticia emocionante!")
