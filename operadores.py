14d32852316a58de131cdd71b5eb1204cc22e92c
JoseUrdiales <49536509+JoseUrdiales@users.noreply.github.com>
2024-02-01 18:39:33 -0500

    Add files via upload

--- operadores.py
+++ operadores.py
@@ -1,4 +1,4 @@
-# Ejemplo de Map, transformo una lista de números a sus cuadrados
+# Ejemplo de Map, transforma una lista de números a sus cuadrados
 numeros = [1, 2, 3, 4, 5]
 cuadrados = map(lambda x: x ** 2, numeros)
 print(list(cuadrados))
@@ -6,4 +6,11 @@ print(list(cuadrados))
 # Ejemplo de filter, filtra números pares de una lista
 numeros = [1, 2, 3, 4, 5, 6]
 pares = filter(lambda x: x % 2 == 0, numeros)
-print(list(pares))  # Output: [2, 4]
+print(list(pares))
+
+# Ejemplo de reduce, suma los elementos de una lista
+from functools import reduce
+numeros = [1, 2, 3, 4, 5]
+suma = reduce(lambda x, y: x + y, numeros)
+print(suma)
+
